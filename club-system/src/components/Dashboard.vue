3d49774b98cf98730c831344e79423f38cc4be05
Petr Herian (Jr.) <petr.herian1@newtonmedia.cz>
2024-10-04 10:51:38 +0200

    deleted useless components

---
+++ club-system/src/components/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="dashboard">
+  <p> Dashboard </p>
+</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.dashboard {
+  width: 100%;
+  min-height: 100%;
+  display: flex;
+  justify-content: center;
+  background-color: blue;
+}
+</style>
