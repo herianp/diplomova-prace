14094fcbbe0c9aa944128668379db9f0c6e71126
Petr Herian (Jr.) <petr.herian1@newtonmedia.cz>
2024-10-31 12:33:18 +0100

    added votes logic

--- club-system/src/components/base/d-card.vue
+++ club-system/src/components/base/d-card.vue
@@ -1,20 +1,88 @@
 <template>
   <div class="card" style="width: 18rem;">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <div class="card-custom-header">
+        <h5 class="card-title">{{survey.title}}</h5>
+        <div class="card-statistics">
+          <h5>Votes: {{survey.votes.length}}</h5>
+          <h5 style="color: gray">
+            <span style="color: green">{{positiveVotes}}</span>
+            |
+            <span style="color: red">{{negativeVotes}}</span>
+          </h5>
+        </div>
+      </div>
+      <p class="card-text">{{survey.description}}</p>
       <div class="d-flex justify-content-end">
-        <a href="#" class="btn btn-primary me-2">Going</a>
-        <a href="#" class="btn btn-primary">Not going</a>
+        <a
+            class="btn btn-primary me-2"
+            :class="{
+              'btn btn-success me-2': isActive && isActivePositive,
+              'btn btn-primary me-2': !isActive
+            }"
+            @click="clubStore.addVote(survey.id, user.id, true)"
+        >
+          Going</a>
+        <a
+            class="btn btn-primary"
+            :class="{
+              'btn btn-danger me-2': isActive && !isActivePositive,
+              'btn btn-primary': !isActive
+            }"
+            @click="clubStore.addVote(survey.id, user.id, false)"
+        >
+          Not going</a>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import {useClubStore} from "@/stores/club.js";
+import {useUserStore} from "@/stores/user.js";
+import {computed} from "vue";
 
+const props = defineProps({
+  surveyId: {
+    type: Number,
+    default: 0
+  }
+});
+
+const clubStore = useClubStore();
+const userStore = useUserStore();
+
+const user = computed(() => userStore.user);
+const survey = computed(() => clubStore.getSurveyById(props.surveyId));
+const positiveVotes = computed(() => clubStore.getPositiveVotes(survey.value.id));
+const negativeVotes = computed(() => clubStore.getNegativeVotes(survey.value.id));
+
+
+const isActive = computed(() => {
+  return survey.value.votes.some(vote => vote.user.id === userStore.user.id);
+});
+
+const isActivePositive = computed(() => {
+  return survey.value.votes.some(vote => {
+    if (vote.user.id === userStore.user.id) {
+      return vote.vote;
+    }
+  });
+});
 </script>
 
 <style scoped>
-
+.card {
+  width: 100% !important;
+}
+.card-custom-header {
+  display: flex;
+  justify-content: space-between;
+}
+.card-statistics{
+  display: flex;
+  flex-direction: column;
+  align-items: end;
+  width: 100px;
+}
 </style>
--- club-system/src/pages/SurveyPage.vue
+++ club-system/src/pages/SurveyPage.vue
@@ -1,22 +1,33 @@
-<script setup>
-
-import DCard from "@/components/base/d-card.vue";
-</script>
-
 <template>
   <div class="container">
     <h1>Survey</h1>
-    <d-card />
+    <d-card
+        v-for="survey in clubStore.activeSurveys"
+        :key="survey.id"
+        :surveyId="survey.id"
+        class="card"
+    />
   </div>
 </template>
+
+<script setup>
+import DCard from "@/components/base/d-card.vue";
+import {useClubStore} from "@/stores/club.js";
+
+const clubStore = useClubStore();
+</script>
 
 <style scoped>
 .container {
   display: flex;
   justify-content: start;
   align-items: center;
-  height: 100vh;
+  height: 100%;
   margin-top: 15px;
+  padding-bottom: 50px;
   flex-direction: column;
+}
+.card {
+  margin-top: 15px;
 }
 </style>
