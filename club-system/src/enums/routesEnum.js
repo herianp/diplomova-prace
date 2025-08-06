7e0fdfae07234323e204987682e4a98b7230002a
petah-pc <peta.herian@seznam.cz>
2025-08-06 09:20:41 +0200

    delted old project

--- club-system/src/enums/routesEnum.js
+++
@@ -1,8 +0,0 @@
-export const RouteEnum = Object.freeze({
-    HOME: { path: '/', name: 'home' },
-    ABOUT: { path: '/about', name: 'about' },
-    DASHBOARD: { path: '/dashboard', name: 'dashboard' },
-    SURVEY: { path: '/survey', name: 'survey' },
-    AUTH: { path: '/auth', name: 'auth' },
-    TEAM: { path: '/team', name: 'team'},
-});
